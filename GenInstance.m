ef8d1728a26ecfa35dd976b3938bea6dd10673d2
Zhenjiang Wang <wangzj_0826@163.com>
2015-03-31 08:57:35 +0800

    update

---
+++ GenInstance.m
@@ -0,0 +1,33 @@
+%% [T,Jm] = GenInstance(PNumber,MNumber,InsNumber)
+% 生成实例，非重入。使用离散均匀分布生成加工工序时间和工件加工机器号
+% 参数说明：
+% PNumber       - 工件数量
+% MNumber       - 机器或每个工件工序数量
+% InsNumber     - 实例数量
+% T             - 各工件各工序使用的时间矩阵集合
+% Jm            - 各工件各工序使用的机器矩阵集合
+%%
+function [T,Jm] = GenInstance(PNumber,MNumber,InsNumber)
+
+T = zeros(PNumber,MNumber,InsNumber);
+Jm = zeros(PNumber,MNumber,InsNumber);
+for i = 1 : InsNumber
+    % 工序时间矩阵
+    t = unidrnd(100,PNumber,MNumber);           % 离散随机分布
+    T(:,:,i) = t;
+    % 机器矩阵
+    jm = zeros(PNumber,MNumber);
+    for j = 1 : PNumber                         % 工件遍历
+        jmTemp =zeros(1,MNumber);
+        for k = 1 : MNumber                     % 机器遍历
+            jmVal = unidrnd(MNumber);
+            while(jmTemp(1,jmVal) == 1)         % 判断该机器号是否已被分配
+                jmVal = unidrnd(MNumber);
+            end
+            jm(j,k) = jmVal;
+            jmTemp(1,jmVal) = 1;                % 标记已分配的机器号
+        end
+    end
+    Jm(:,:,i) = jm;
+end
+end
---
+++ Main.m
@@ -0,0 +1,6 @@
+clear all
+clc
+
+
+
+[T,Jm] = GenInstance(10,10,200);
